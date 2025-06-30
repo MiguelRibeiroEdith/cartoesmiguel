2e7717926d54a2c73003fd1fabaaa8e90138e02e
MiguelRibeiroEdith <thadeu.miguel@escola.pr.gov.br>
2025-06-30 10:43:13 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,39 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é  uma linguagem de programação',
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris',
+)
+
+criaCartao(
+    'Programação',
+    'Oque e uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa',
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
+
+
+criaCartao(
+    'Games',
+    'Qual o bloco indestrutivel do minecraft',
+    'Bedrock',
+)
+
+
+criaCartao(
+    'Games',
+    'Quantos mapas exieste no valorant',
+    '12',
+)
+
+
